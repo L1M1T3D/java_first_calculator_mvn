7ae6eb742838de48732d43ab795480d52af2766a
sapphir0v <6.sapphirov.6@bk.ru>
2024-12-08 03:06:49 +0300

    git init

---
+++ src/test/java/me/sapphir0v/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sapphir0v.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
